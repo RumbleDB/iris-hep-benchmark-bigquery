19c377d5e0d5037cf78b688e8348a8e80cae98d1
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2020-03-29 22:26:31 +0200

    Add first implementations of HEP benchmark query 5.

---
+++ q5.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from google.cloud import bigquery
+from matplotlib import pyplot as plt
+
+client = bigquery.Client()
+query = """
+    SELECT
+      CASE WHEN MET_sumet < 0 THEN 0
+           WHEN MET_sumet > 2000 THEN 100
+           ELSE CAST(MET_sumet / 20 AS INT64) END * 20 AS MET_sumet_rounded,
+      COUNT(*) AS count
+    FROM root_playground.Run2012B_SingleMu_small_JetsMuons
+    WHERE nMuon >= 2 AND
+      (SELECT COUNT(*) AS mass
+       FROM UNNEST(Muon) m1 WITH OFFSET i
+       CROSS JOIN UNNEST(Muon) m2 WITH OFFSET j
+       WHERE
+         m1.charge <> m2.charge AND i < j AND
+         SQRT(2*m1.pt*m2.pt*(COSH(m1.eta-m2.eta)-COS(m1.phi-m2.phi))) BETWEEN 60 AND 100) > 0
+    GROUP BY MET_sumet_rounded
+    ORDER BY MET_sumet_rounded
+    """
+query_job = client.query(query)
+df = query_job.to_dataframe()
+
+plt.hist(df['MET_sumet_rounded'], bins=len(df.index), weights=df['count'])
+plt.show()
--- view.py
+++ view.py
@@ -10,7 +10,25 @@ SELECT *,
           Jet_puId.list[OFFSET(i)].element AS puId,
           Jet_btag.list[OFFSET(i)].element AS btag
         FROM UNNEST(Jet_pt.list)WITH OFFSET i
-        ) AS Jet
-FROM root_playground.Run2012B_SingleMu_small
+        ) AS Jet,
+  ARRAY(SELECT AS STRUCT
+          Muon_pt.list[OFFSET(i)].element AS pt,
+          Muon_eta.list[OFFSET(i)].element AS eta,
+          Muon_phi.list[OFFSET(i)].element AS phi,
+          Muon_mass.list[OFFSET(i)].element AS mass,
+          Muon_charge.list[OFFSET(i)].element AS charge,
+          Muon_pfRelIso03_all.list[OFFSET(i)].element AS pfRelIso03_all,
+          Muon_pfRelIso04_all.list[OFFSET(i)].element AS pfRelIso04_all,
+          Muon_tightId.list[OFFSET(i)].element AS tightId,
+          Muon_softId.list[OFFSET(i)].element AS softId,
+          Muon_dxy.list[OFFSET(i)].element AS dxy,
+          Muon_dxyErr.list[OFFSET(i)].element AS dxyErr,
+          Muon_dz.list[OFFSET(i)].element AS dz,
+          Muon_dzErr.list[OFFSET(i)].element AS dzErr,
+          Muon_jetIdx.list[OFFSET(i)].element AS jetIdx,
+          Muon_genPartIdx.list[OFFSET(i)].element AS genPartIdx
+        FROM UNNEST(Muon_pt.list)WITH OFFSET i
+        ) AS Muon
+FROM cloud-shared-execution.root_playground.Run2012B_SingleMu_small
 CROSS JOIN UNNEST(Jet_pt.list) AS Jet_pt_list
 """
