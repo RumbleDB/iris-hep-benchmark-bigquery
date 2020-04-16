f62e7e81f7a5e61235830e3d990e042016070121
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2020-04-16 10:25:21 +0200

    Extend view to represent Electrons as structs.

--- view.sql
+++ view.sql
@@ -6,7 +6,7 @@ SELECT *,
           Jet_mass.list[OFFSET(i)].element AS mass,
           Jet_puId.list[OFFSET(i)].element AS puId,
           Jet_btag.list[OFFSET(i)].element AS btag
-        FROM UNNEST(Jet_pt.list)WITH OFFSET i
+        FROM UNNEST(Jet_pt.list) WITH OFFSET i
         ) AS Jet,
   ARRAY(SELECT AS STRUCT
           Muon_pt.list[OFFSET(i)].element AS pt,
@@ -24,6 +24,23 @@ SELECT *,
           Muon_dzErr.list[OFFSET(i)].element AS dzErr,
           Muon_jetIdx.list[OFFSET(i)].element AS jetIdx,
           Muon_genPartIdx.list[OFFSET(i)].element AS genPartIdx
-        FROM UNNEST(Muon_pt.list)WITH OFFSET i
-        ) AS Muon
+        FROM UNNEST(Muon_pt.list) WITH OFFSET i
+        ) AS Muon,
+  ARRAY(SELECT AS STRUCT
+          Electron_pt.list[OFFSET(i)].element AS pt,
+          Electron_eta.list[OFFSET(i)].element AS eta,
+          Electron_phi.list[OFFSET(i)].element AS phi,
+          Electron_mass.list[OFFSET(i)].element AS mass,
+          Electron_charge.list[OFFSET(i)].element AS charge,
+          Electron_pfRelIso03_all.list[OFFSET(i)].element AS pfRelIso03_all,
+          Electron_dxy.list[OFFSET(i)].element AS dxy,
+          Electron_dxyErr.list[OFFSET(i)].element AS dxyErr,
+          Electron_dz.list[OFFSET(i)].element AS dz,
+          Electron_dzErr.list[OFFSET(i)].element AS dzErr,
+          Electron_cutBasedId.list[OFFSET(i)].element AS cutBasedId,
+          Electron_pfId.list[OFFSET(i)].element AS pfId,
+          Electron_jetIdx.list[OFFSET(i)].element AS jetIdx,
+          Electron_genPartIdx.list[OFFSET(i)].element AS genPartIdx
+        FROM UNNEST(Electron_pt.list) WITH OFFSET i
+        ) AS Electron
 FROM cloud-shared-execution.root_playground.Run2012B_SingleMu_small
