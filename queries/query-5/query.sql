998d1960164c286f0e9db9013afcc7572246a4c6
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2021-04-02 18:26:40 +0200

    Fix computation of invatiant mass (fix regression from 6c10e2aa6).

--- queries/query-5/query.sql
+++ queries/query-5/query.sql
@@ -8,6 +8,6 @@ WHERE ARRAY_LENGTH(Muon) >= 2 AND
    CROSS JOIN UNNEST(Muon) m2 WITH OFFSET j
    WHERE
      m1.charge <> m2.charge AND i < j AND
-     2*m1.pt*m2.pt*(COSH(m1.eta-m2.eta)-COS(m1.phi-m2.phi)) BETWEEN 60 AND 120) > 0
+     SQRT(2*m1.pt*m2.pt*(COSH(m1.eta-m2.eta)-COS(m1.phi-m2.phi))) BETWEEN 60 AND 120) > 0
 GROUP BY x
 ORDER BY x
